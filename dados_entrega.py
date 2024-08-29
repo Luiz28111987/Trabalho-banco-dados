b3de07fba5b8eeda299d2829383e3e328292026c
Luiz Fernando <165337963+Luiz28111987@users.noreply.github.com>
2024-08-29 09:04:48 -0300

    banco de dados

--- dados_entrega.py
+++ dados_entrega.py
@@ -5,8 +5,8 @@ from sqlalchemy import create_engine
 file_path = "C:/Users/Luiz/Desktop/Estudos/Trabalho-banco-dados/CONTROLE_ENTREGA_JULHO_2024_MOTORISTAS.xlsx"
 
 # Cabeçalho personalizado para o DataFrame
-cabecalho = ['MOTORISTA','Placa','Data_Entrega','Hora_Saida','KM_Inicial',
-             'Quantidade_Notas_Fiscais','Quantidade_Coletas','KM_Final','TOTAL_DIA','REGIAO']
+cabecalho = ['motorista','placa','data_entrega','hora_saida','km_inicial',
+             'quantidade_notas_fiscais','quantidade_coletas','km_final','total_dia','regiao']
 
 # Verifica se o arquivo existe no caminho especificado
 try:
@@ -15,80 +15,79 @@ except FileNotFoundError as e:
     print(f"Erro: {e}")
     df = pd.DataFrame()  # cria um DataFrame vazio para evitar erros subsequentes
 
+# Adicionando uma nova coluna na posição desejada
+df.insert(0, 'numero_entrega', range(1, len(df) + 1))
+
 # Formatar a data para outro formato ( 08/11/2018 - 2018/11/08)
-df['Data_Entrega'] = pd.to_datetime(df['Data_Entrega'], format='%d/%m/%Y').dt.strftime('%Y/%m/%d')
-df['Data_Entrega'] = pd.to_datetime(df['Data_Entrega'])
+df['data_entrega'] = pd.to_datetime(df['data_entrega'], format='%d/%m/%Y').dt.strftime('%Y/%m/%d')
+df['data_entrega'] = pd.to_datetime(df['data_entrega'])
 
 # Extrair apenas a data (sem a parte do tempo)
-df['Data_Entrega'] = df['Data_Entrega'].dt.date
+df['data_entrega'] = df['data_entrega'].dt.date
 
 # Preenchemos o valor NA por ( 0 )
 # inplace = True / para altera o proprio DataFrame
 dado = 0
-df['Quantidade_Coletas'] = df['Quantidade_Coletas'].fillna(value = dado)
+df['quantidade_coletas'] = df['quantidade_coletas'].fillna(value = dado)
 
 # Convertendo uma única coluna para inteiro
-df['Quantidade_Coletas'] = df['Quantidade_Coletas'].astype(int)
+df['quantidade_coletas'] = df['quantidade_coletas'].astype(int)
 
-df['Quantidade_Notas_Fiscais'] = df['Quantidade_Notas_Fiscais'].fillna(value = dado)
+df['quantidade_notas_fiscais'] = df['quantidade_notas_fiscais'].fillna(value = dado)
 
 # Convertendo uma única coluna para inteiro
-df['Quantidade_Notas_Fiscais'] = df['Quantidade_Notas_Fiscais'].astype(int)
+df['quantidade_notas_fiscais'] = df['quantidade_notas_fiscais'].astype(int)
+
 
 # Substituindo nomes das regiões
-df['REGIAO'] = df['REGIAO'].str.replace('SOB', 'SOBRADINHO')
-df['REGIAO'] = df['REGIAO'].str.replace('PLANA-DF', 'PLANALTINA-DF')
-df['REGIAO'] = df['REGIAO'].str.replace('VICENTE P', 'VICENTE PIRES')
-df['REGIAO'] = df['REGIAO'].str.replace('CEI', 'CEILANDIA')
-df['REGIAO'] = df['REGIAO'].str.replace('PARQUE W', 'PARK WAY')
-df['REGIAO'] = df['REGIAO'].str.replace('CIDADE O', 'CIDADE OCIDENTAL')
-df['REGIAO'] = df['REGIAO'].str.replace('TAGUA-SUL', 'TAGUATINGA-SUL')
-df['REGIAO'] = df['REGIAO'].str.replace('S SEB', 'SAO SEBASTIAO')
-df['REGIAO'] = df['REGIAO'].str.replace('SAI', 'SIA')
-df['REGIAO'] = df['REGIAO'].str.replace('SANTA M', 'SANTA MARIA')
-df['REGIAO'] = df['REGIAO'].str.replace('RIACHO F', 'RIACHO FUNDO')
-df['REGIAO'] = df['REGIAO'].str.replace('PLANA-GO', 'PLANALTINA-GO')
-df['REGIAO'] = df['REGIAO'].str.replace('BRASLÂNDIA', 'BRAZLANDIA')
-df['REGIAO'] = df['REGIAO'].str.replace('TAGUA-NORTE', 'TAGUATINGA-NORTE')
-df['REGIAO'] = df['REGIAO'].str.replace('V PIRES', 'VICENTE PIRES')
-df['REGIAO'] = df['REGIAO'].str.replace('A LINDAS', 'AGUAS LINDAS')
-df['REGIAO'] = df['REGIAO'].str.replace('TAGUA NORTE', 'TAGUATINGA-NORTE')
-df['REGIAO'] = df['REGIAO'].str.replace('PLANALTINA-DF-GO', 'PLANALTINA-DF/PLANLTINA-GO')
-df['REGIAO'] = df['REGIAO'].str.replace('CANDANGO', 'CANDANGOLANDIA')
-df['REGIAO'] = df['REGIAO'].str.replace('CANDANGA', 'CANDANGOLANDIA')
-df['REGIAO'] = df['REGIAO'].str.replace('NUCLEO B', 'NUCLEO BANDEIRANTE')
-df['REGIAO'] = df['REGIAO'].str.replace('SICIA', 'SCIA')
-df['REGIAO'] = df['REGIAO'].str.replace('VAL', 'VALPARAISO-GO')
-df['REGIAO'] = df['REGIAO'].str.replace('SOF-NORTE-SUL', 'SOF-NORTE/SOF-SUL')
-df['REGIAO'] = df['REGIAO'].str.replace('TAGUATINGA-NORTE-SUL', 'TAGUATINGA-NORTE/TAGUATINGA-SUL')
-df['REGIAO'] = df['REGIAO'].str.replace('CIDADE A', 'CIDADE DO AUTOMOVEL')
+df['regiao'] = df['regiao'].str.replace('SOB', 'SOBRADINHO')
+df['regiao'] = df['regiao'].str.replace('PLANA-DF', 'PLANALTINA-DF')
+df['regiao'] = df['regiao'].str.replace('VICENTE P', 'VICENTE PIRES')
+df['regiao'] = df['regiao'].str.replace('CEI', 'CEILANDIA')
+df['regiao'] = df['regiao'].str.replace('PARQUE W', 'PARK WAY')
+df['regiao'] = df['regiao'].str.replace('CIDADE O', 'CIDADE OCIDENTAL')
+df['regiao'] = df['regiao'].str.replace('TAGUA-SUL', 'TAGUATINGA-SUL')
+df['regiao'] = df['regiao'].str.replace('S SEB', 'SAO SEBASTIAO')
+df['regiao'] = df['regiao'].str.replace('SAI', 'SIA')
+df['regiao'] = df['regiao'].str.replace('SANTA M', 'SANTA MARIA')
+df['regiao'] = df['regiao'].str.replace('RIACHO F', 'RIACHO FUNDO')
+df['regiao'] = df['regiao'].str.replace('PLANA-GO', 'PLANALTINA-GO')
+df['regiao'] = df['regiao'].str.replace('BRASLÂNDIA', 'BRAZLANDIA')
+df['regiao'] = df['regiao'].str.replace('TAGUA-NORTE', 'TAGUATINGA-NORTE')
+df['regiao'] = df['regiao'].str.replace('V PIRES', 'VICENTE PIRES')
+df['regiao'] = df['regiao'].str.replace('A LINDAS', 'AGUAS LINDAS')
+df['regiao'] = df['regiao'].str.replace('TAGUA NORTE', 'TAGUATINGA-NORTE')
+df['regiao'] = df['regiao'].str.replace('PLANALTINA-DF-GO', 'PLANALTINA-DF/PLANLTINA-GO')
+df['regiao'] = df['regiao'].str.replace('CANDANGO', 'CANDANGOLANDIA')
+df['regiao'] = df['regiao'].str.replace('CANDANGA', 'CANDANGOLANDIA')
+df['regiao'] = df['regiao'].str.replace('NUCLEO B', 'NUCLEO BANDEIRANTE')
+df['regiao'] = df['regiao'].str.replace('SICIA', 'SCIA')
+df['regiao'] = df['regiao'].str.replace('VAL', 'VALPARAISO-GO')
+df['regiao'] = df['regiao'].str.replace('SOF-NORTE-SUL', 'SOF-NORTE/SOF-SUL')
+df['regiao'] = df['regiao'].str.replace('TAGUATINGA-NORTE-SUL', 'TAGUATINGA-NORTE/TAGUATINGA-SUL')
+df['regiao'] = df['regiao'].str.replace('CIDADE A', 'CIDADE DO AUTOMOVEL')
 
 # Obtendo os valores únicos
-# unique_names = df['REGIAO'].unique()
+# unique_names = df['regiao'].unique()
 
 # print(unique_names)
 
 # Divida a Coluna em Listas
-df['REGIAO'] = df['REGIAO'].str.split('/')
+df['regiao'] = df['regiao'].str.split('/')
 
 # Transformar Listas em Linhas Separadas
-df_exploded = df.explode('REGIAO')
+df_exploded = df.explode('regiao')
 
 # Remova Espaços em Branco Extras
-df_exploded['REGIAO'] = df_exploded['REGIAO'].str.strip()
+df_exploded['regiao'] = df_exploded['regiao'].str.strip()
 
-print(df_exploded)
-
-"""
 try:
     # Criar uma conexão com o banco de dados PostgreSQL
     engine = create_engine('postgresql://postgres:1234@localhost:5432/trabalho_extensao')
 
     # Importar dados para a tabela
-    df.to_sql('dados', engine, if_exists='append', index=False)
+    df_exploded.to_sql('dados', engine, if_exists='append', index=False)
     print("Dados importados com sucesso.")
 
 except Exception as e:
     print(f"Erro ao conectar ao banco de dados ou importar dados: {e}")
-
-"""
